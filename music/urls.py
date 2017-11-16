93ce397025c606648d60c217b0073fbd1f9c29f6
Staartvin <mijnleraar@msn.com>
2017-11-16 21:44:52 +0100

    Make a beautiful website

--- music/urls.py
+++ music/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),  # Default page
     # Matches /music/albumID/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),  # /music/:albumid
+    # Matches /music/albumID/favorite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),  # /music/:albumid
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
-from django.http import Http404
 
 
 # Create your views here.
@@ -11,8 +10,7 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(id=album_id)
-    except Album.DoesNotExist:
-        raise Http404("The album with id " + album_id + " does not exist!")
+    album = get_object_or_404(Album, id=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+
